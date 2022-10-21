4d606087b0379eecc9d6fdc341b9c79d1c0e6cac
Juan Rios <jriossdev@gmail.com>
2022-10-21 16:58:18 -0500

    ejercicios1

--- ejercicios/ejercicios1/ejer2.js
+++ ejercicios/ejercicios1/ejer2.js
@@ -6,14 +6,65 @@
 
 console.log("---1---")
 
-function invertir(text){
+const invertirTexto = (text = '') =>
+  (!text)
+    ? console.warn("No ingresaste un cadena de texto")
+    : console.log(text.split('').reverse().join(''))
 
-  let invertido = text.split('')
+invertirTexto()
+invertirTexto("Hola Mundo")
+invertirTexto("javascript es un lenguaje de programacion increible")
+
+console.log("---1--- ")
+
+console.log("---2--- ")
+
+const contarText = (text = '', textIncluido = undefined) =>
+  (!text)
+    ? console.warn("No ingresaste ningun texto")
+    : (textIncluido === undefined)
+      ? console.warn("No ingresaste ninguna palabra para buscar")
+      : console.log(text.split(' ').reduce((a,b) => (a[b] ? a[b] +=1 : a[b] = 1 ,a), {}))
+
+contarText("hola mundo adios mundo mundo cruel", "mundo")
+console.log("---2--- ")
+
+console.log("-----Solucion del profe 2 ------")
+
+
+const textEnCadena = (cadena = '', text  = '') =>{
   
-  console.log(text)
+  if(!cadena) return console.warn("No ingresaste ningun texto")
+  if(!text) return console.warn("No ingresaste el texto a contar")
+  
+  let i = 0
+  contador = 0
+  
+  while(i !== -1){
+    i = cadena.indexOf(text, i)
+    if(i !== -1){
+      i++
+      contador++
+    }
+  }
+  return console.log(`La palabra ${text} esta incluida ${contador} veces`)
   
 }
+textEnCadena("mundo mundo mundo mundo mundo", "mundo")
+console.log("-----Solucion del profe 2 ------")
 
-invertir("hola mundo")
 
-console.log("---1---")
+
+const palindromo = (text = "") =>{
+  if(!text)  return console.warn("No ingresaste ningun texto")
+  const text1 = text.split('').reverse().join('')
+  const text2 = text1.split('').reverse().join('')
+  // if(text1 === text2){
+  //   return console.log("Es un pálindromo")
+  // }else{
+  //   return console.log("No es un palindromo")
+  // }
+  console.log(text1)
+  console.log(text2)
+}
+palindromo("yo soy")
