004663ce509accc2febe95d31b95948999d1d6dc
Juan Rios <jriossdev@gmail.com>
2022-10-25 17:36:43 -0500

    segundo ejercici

--- ejercicios/ejercicios1/ejer2.js
+++ ejercicios/ejercicios1/ejer2.js
@@ -47,24 +47,70 @@ const textEnCadena = (cadena = '', text  = '') =>{
       contador++
     }
   }
-  return console.log(`La palabra ${text} esta incluida ${contador} veces`)
+  return console.log(`La palabra "${text}" esta incluida ${contador} veces`)
   
 }
 textEnCadena("mundo mundo mundo mundo mundo", "mundo")
 console.log("-----Solucion del profe 2 ------")
 
-
-
+console.log("--------3----------")
 const palindromo = (text = "") =>{
+  const str = text
   if(!text)  return console.warn("No ingresaste ningun texto")
-  const text1 = text.split('').reverse().join('')
-  const text2 = text1.split('').reverse().join('')
-  // if(text1 === text2){
-  //   return console.log("Es un pálindromo")
-  // }else{
-  //   return console.log("No es un palindromo")
-  // }
-  console.log(text1)
-  console.log(text2)
+  text = text.replace(/\s/g, '').toLocaleLowerCase()
+  const separar = text.split('')
+  const reverse = separar.reverse()
+  const juntar = reverse.join('')
+  const palindrom = (text === juntar) 
+    ? console.log( `La palabra "${str}" Es un polindromo` )
+    : console.log(` La palabra "${str}" No es un polindromo`)
 }
-palindromo("yo soy")
+palindromo()
+palindromo("wppwko ki")
+palindromo("Ateo poco poeta") 
+console.log("--------3----------")
+
+console.log("--------4----------")
+
+const eliminarCaracteres = (text = '', caracter = '') =>{
+  if(!text) return console.warn("No ingresaste ningun texto")
+  if(!caracter) return console.warn("No ingresaste ningun carater a eliminar")
+
+  const textSep = text.split('')
+  const caracterSep = caracter.split('')
+  
+  let arr = []
+  for(let i = 0; i < textSep.length; i++){
+    let igual =  false
+    for(let j = 0; j < caracterSep.length; j++){
+      if(textSep[i] === caracterSep[j]){
+        igual = true
+      }
+    }
+    if(!igual)arr.push(textSep[i]) 
+  }
+  
+  return console.log(arr.join(''))
+}
+
+eliminarCaracteres()
+eliminarCaracteres("xyz1 xyz2 xyz3 xyz4 xyz5", "xyz")
+
+
+
+
+console.log("--------4----------")
+console.log("--------Solucion del profe 4----------")
+
+const eliminarPatron = (text = '', patron = '') => {
+
+  (!text)
+    ? console.log("No ingresaste el texto")
+    : (!patron)
+      ? console.log("No ingresaste el patron")
+      : console.log(text.replace(new RegExp(patron, "ig"), ""))
+
+}
+
+eliminarPatron("xyz1 xyz2 xyz3 xyz4 xyz5", "xyz")
+console.log("--------Solucion del profe 4----------")
